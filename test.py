61f2ffb6185d6d4aaf41724175c3742848f0d3f4
thoriqtau <thoriqtau@gmail.com>
2026-09-25 14:28:51 +0700

    app added

--- test.py
+++ test.py
@@ -1,8 +1,19 @@
 from tools.tavily_tool import tavily_search
 from tools.flight_tool import search_flights
+from backend import run_travel_agent
 
 # res = tavily_search("Best hotels in Indonesia")
 # print(res)
 
-res = search_flights("Rencanakan perjalanan 7 hari ke Australia dari Indonesia")
-print(res)
+# res = search_flights("Rencanakan perjalanan 7 hari ke Australia dari Indonesia")
+# print(res)
+
+user_input = input("Enter travel request: ")
+
+response = run_travel_agent(
+    user_input,
+    thread_id="test_user"
+)
+
+print("\n FINAL RESPONSE:\n")
+print(response["answer"])
